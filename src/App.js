8001eb9b87cd586dca207215747ebe9608cca819
rezafikkri <fikkri.reza@gmail.com>
2022-11-20 11:10:16 +0700

    feat: add todo item and show list todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
+import List from './list.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const todoItem = e.target.querySelector('input').value;
+    this.setState(state => ({
+      items: [...state.items, todoItem]
+    }));
+    e.target.reset();
+  }
+  
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" />
+          <button type="submit">Add</button>
+        </form>
+
+        <List items={this.state.items} />
+      </div>
+    );
+  } 
 }
 
 export default App;
